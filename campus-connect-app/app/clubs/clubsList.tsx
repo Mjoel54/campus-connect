580c96d1b02ff66f99cd4e8281f53ce3aef3d3e6
Mitch <mitchjoelklein@hotmail.com>
2025-03-18 09:28:45 +1100

    club page route

--- campus-connect-app/app/clubs/clubsList.tsx
+++ campus-connect-app/app/clubs/clubsList.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { Club } from "../club-data";
 
 export default function ClubsList({ clubs }: { clubs: Club[] }) {
@@ -6,17 +7,19 @@ export default function ClubsList({ clubs }: { clubs: Club[] }) {
     <>
       {clubs.map((club) => {
         return (
-          <div key={club.id}>
-            <Image
-              src={"/skiing.jpg"}
-              alt="person skiing"
-              width={200}
-              height={200}
-            />
-            <h2>{club.name}</h2>
-            <p>{club.description}</p>
-            <p>Members: {club.members.join(", ")}</p>
-          </div>
+          <Link key={club.id} href={`/clubs/${club.id}`} passHref>
+            <div>
+              <Image
+                src={"/skiing.jpg"}
+                alt="person skiing"
+                width={200}
+                height={200}
+              />
+              <h2>{club.name}</h2>
+              <p>{club.description}</p>
+              <p>Members: {club.members.join(", ")}</p>
+            </div>
+          </Link>
         );
       })}
     </>
--- campus-connect-app/app/clubs/page.tsx
+++ campus-connect-app/app/clubs/page.tsx
@@ -1,7 +1,7 @@
 import ClubsList from "./clubsList";
 import { clubs } from "../club-data";
 
-export default function Clubs() {
+export default function ClubsPage() {
   return (
     <>
       <h1>Clubs</h1>
