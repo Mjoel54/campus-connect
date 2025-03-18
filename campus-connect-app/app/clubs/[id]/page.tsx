ca413b7fabebb254d81476bdefbc14e1a6feeda4
Mitch <mitchjoelklein@hotmail.com>
2025-03-18 11:03:44 +1100

    NotFound if no club

--- campus-connect-app/app/clubs/[id]/page.tsx
+++ campus-connect-app/app/clubs/[id]/page.tsx
@@ -1,7 +1,12 @@
+import NotFound from "@/app/not-found";
 import { clubs } from "../../club-data";
 
 export default function ClubPage({ params }: { params: { id: string } }) {
   const club = clubs.find((club) => club.id === params.id);
+
+  if (!club) {
+    return <NotFound />;
+  }
 
   return (
     <>
