dfaa220c5fe8d93f33fe7217bf13c70ce5a96e92
Mitch <mitchjoelklein@hotmail.com>
2025-03-19 14:55:29 +1100

    ClubsList uses MongoDB data

--- campus-connect-app/app/clubs/ClubCard.tsx
+++ campus-connect-app/app/clubs/ClubCard.tsx
@@ -61,44 +61,41 @@ const people = [
   },
 ];
 
-export default function ClubCard() {
+export default async function ClubCard() {
+  const response = await fetch("http://localhost:3000/api/clubs");
+  const data = await response.json();
+  console.log(data);
+
   return (
     <ul
       role="list"
       className="divide-y divide-gray-100 overflow-hidden bg-white ring-1 shadow-xs ring-gray-900/5 sm:rounded-xl"
     >
-      {people.map((person) => (
+      {data.map((club) => (
         <li
-          key={person.email}
-          className="relative flex justify-between gap-x-6 px-4 py-5 hover:bg-gray-50 sm:px-6"
+          key={club._id}
+          className="relative flex justify-between gap-x-6 px-4 py-5 hover:bg-gray-50 sm:px-6 cursor-pointer"
         >
           <div className="flex min-w-0 gap-x-4">
-            <img
+            {/* <img
               alt=""
               src={person.imageUrl}
               className="size-12 flex-none rounded-full bg-gray-50"
-            />
+            /> */}
             <div className="min-w-0 flex-auto">
               <p className="text-sm/6 font-semibold text-gray-900">
-                <a href={person.href}>
+                <a href={`/clubs/${club.id}`} className="hover:underline">
                   <span className="absolute inset-x-0 -top-px bottom-0" />
-                  {person.name}
+                  {club.name}
                 </a>
               </p>
-              <p className="mt-1 flex text-xs/5 text-gray-500">
-                <a
-                  href={`mailto:${person.email}`}
-                  className="relative truncate hover:underline"
-                >
-                  {person.email}
-                </a>
-              </p>
+              <p className="mt-1 flex text-xs/5 text-gray-500">Category</p>
             </div>
           </div>
           <div className="flex shrink-0 items-center gap-x-4">
             <div className="hidden sm:flex sm:flex-col sm:items-end">
-              <p className="text-sm/6 text-gray-900">{person.role}</p>
-              {person.lastSeen ? (
+              <p className="text-sm/6 text-gray-900"># members</p>
+              {/* {person.lastSeen ? (
                 <p className="mt-1 text-xs/5 text-gray-500">
                   Last seen{" "}
                   <time dateTime={person.lastSeenDateTime}>
@@ -112,7 +109,7 @@ export default function ClubCard() {
                   </div>
                   <p className="text-xs/5 text-gray-500">Online</p>
                 </div>
-              )}
+              )} */}
             </div>
             <ChevronRightIcon
               aria-hidden="true"
